f36437ec37eceb80091859611894b16866ede6ad
Jason Talon <jason.talon@outlook.com>
2019-12-05 22:13:15 +0800

    enhancements

--- pages/index.vue
+++ pages/index.vue
@@ -98,121 +98,215 @@
           >Search</v-btn
         ></v-row
       >
-      <v-row class="align-stretch justify-space-around">
-        <v-data-table
-          :headers="events.headers"
-          :items="events.items"
-          item-key="name"
-          show-select
-          :items-per-page="500"
-          hide-default-footer
-          disable-pagination
-          loading-text="Searching..."
-          class="elevation-1"
-          dense
-          :loading="events.loading"
-        >
-          <template v-slot:item.host.name="{ item }">
-            <template v-if="item.host">
-              <v-row dense>
-                <v-col cols="2" class="flex-shrink-1">
-                  <v-tooltip bottom>
-                    <template v-slot:activator="{ on }">
-                      <v-btn
-                        x-small
-                        text
-                        @click="findPage({eventId : item.id})"
-                        v-on="on"
-                        ><v-icon small>mdi-information</v-icon></v-btn
-                      ></template>
-                    <span>Find contact information</span>
-                  </v-tooltip>
+      <v-row class="">
+        <v-col cols="12" class="">
+          <v-data-table
+            :headers="events.headers"
+            :items="events.items"
+            item-key="id"
+            :items-per-page="500"
+            hide-default-footer
+            disable-pagination
+            loading-text="Searching..."
+            class="elevation-1"
+            dense
+            :loading="events.loading"
+          >
+            <template v-slot:item.host.name="{ item }">
+              <template v-if="item.host">
+                <v-row dense>
+                  <v-col cols="2" class="flex-shrink-1">
+                    <v-tooltip bottom>
+                      <template v-slot:activator="{ on }">
+                        <v-btn v-if="!item.host.details"
+                          x-small
+                          text
+                          @click="findPage({eventId : item.id})"
+                          v-on="on"
+                          ><v-icon small>mdi-information</v-icon></v-btn
+                        ></template>
+                      <span>Find contact information</span>
+                    </v-tooltip>
 
-                </v-col>
-                <v-col cols="10" class="flex-grow-1">
-                  <a :href="'https://fb.com/' + item.host.id" target="_blank" class="d-block">{{
-                    item.host.name
-                  }}</a>
-                  <v-progress-circular v-if="item.host.loading" size="22" width="2"
-                    indeterminate
-                    color="red"
-                  ></v-progress-circular>
-                  </v-col
-                >
-              </v-row>
-                <template v-if="item.host.details" class="d-block">
-                  <v-row v-if="item.host.details.address" dense>
-                    <v-col cols="2" class="d-flex"><v-icon small right>mdi-map-marker</v-icon></v-col>
-                    <v-col cols="10" class="caption">{{item.host.details.address}}</v-col>
-                  </v-row>
-                   <v-row v-if="item.host.details.email" dense>
-                    <v-col cols="2" class="d-flex"><v-icon small right>mdi-email</v-icon></v-col>
-                    <v-col cols="10"  class="caption">{{item.host.details.email}}</v-col>
-                  </v-row>
-                   <v-row v-if="item.host.details.phone" dense>
-                    <v-col cols="2" class="d-flex"><v-icon small right>mdi-phone</v-icon></v-col>
-                    <v-col cols="10"  class="caption">{{item.host.details.phone}}</v-col>
-                  </v-row>
-                   <v-row v-if="item.host.details.website" dense>
-                    <v-col cols="2" class="d-flex"><v-icon small right>mdi-web</v-icon></v-col>
-                    <v-col cols="10"  class="caption">{{item.host.details.website}}</v-col>
-                  </v-row>
-                  </template>
-            </template>
-          </template>
-          <template v-slot:item.date="{ item }">
-            <v-tooltip bottom>
-            <template v-slot:activator="{ on }">
-              <span v-on="on">{{ item.date }}</span>
+                  </v-col>
+                  <v-col cols="10" class="flex-grow-1">
+                    <a :href="'https://fb.com/' + item.host.id" target="_blank" class="d-block">{{
+                      item.host.name
+                    }}</a>
+                      <div v-if="item.host.loading" class="d-flex flex-column">
+                        <span class="caption text-center">Finding contact details...</span>
+                        <v-progress-linear indeterminate ></v-progress-linear>
+                      </div>
+                    </v-col
+                  >
+                </v-row>
+                  <template v-if="item.host.details" class="d-block">
+                    <v-row v-if="item.host.details.address" dense>
+                      <v-col cols="2" class="d-flex"><v-icon small right>mdi-map-marker</v-icon></v-col>
+                      <v-col cols="10" class="caption">
+                        {{item.host.details.address.replace(/get directions/i, '')}}
+                        </v-col>
+                    </v-row>
+                    <v-row v-if="item.host.details.email" dense>
+                      <v-col cols="2" class="d-flex"><v-icon small right>mdi-email</v-icon></v-col>
+                      <v-col cols="10"  class="caption">
+                        <div class="d-block text-truncate">{{item.host.details.email}}</div></v-col>
+                    </v-row>
+                    <v-row v-if="item.host.details.phone" dense>
+                      <v-col cols="2" class="d-flex"><v-icon small right>mdi-phone</v-icon></v-col>
+                      <v-col cols="10"  class="caption">
+                        <div class="d-block text-truncate">{{item.host.details.phone}}</div></v-col>
+                    </v-row>
+                    <v-row v-if="item.host.details.website" dense>
+                      <v-col cols="2" class="d-flex"><v-icon small right>mdi-web</v-icon></v-col>
+                      <v-col cols="10"  class="caption">
+                        <div class="d-block text-truncate" style="max-width:200px;">
+                          <a :href="item.host.details.website" target="_blank">
+                          {{item.host.details.website}}</a></div></v-col>
+                    </v-row>
+                    </template>
               </template>
-            <span>{{item.time}}</span>
-          </v-tooltip>
-          </template>
-          <template v-slot:item.title="{ item }">
-            <a :href="'https://fb.com/' + item.id" target="_blank" class="d-block">{{
-              item.title
-            }}</a>
-            <template v-if="item.socialContext"><div  class="d-block text-left caption">{{item.socialContext}}</div></template>
-
-          </template>
-          <template v-slot:item.location="{ item }">
-            <template v-if="item.location">
-              <a class="d-block" target="_blank" :href="'https://maps.google.com?q=' +  item.location">{{ item.location.split('·')[0].trim() }}</a>
-              <span class="caption">{{ item.location.split('·').pop().trim() }}</span>
             </template>
-          </template>
-          <template v-slot:item.buyTicketHost="{ item }">
-            <template v-if="item.buyTicketHost">
-              <v-icon x-small="">mdi-ticket</v-icon>
-              <a :href="item.buyticketUrl" target="_blank">{{ item.buyTicketHost }}</a>
+            <template v-slot:item.date="{ item }">
+              <v-tooltip bottom>
+              <template v-slot:activator="{ on }">
+                <span v-on="on">{{ item.date }}</span>
+                </template>
+              <span>{{item.time}}</span>
+            </v-tooltip>
             </template>
-            <template v-else-if="item.isFreeEvent">Free</template>
-            <template v-else>n/a</template>
-          </template>
-        </v-data-table>
+            <template v-slot:item.title="{ item }">
+              <a :href="'https://fb.com/' + item.id" target="_blank" class="d-block">{{
+                item.title
+              }}</a>
+              <template v-if="item.socialContext"><div  class="d-block text-left caption">{{item.socialContext}}</div></template>
+            </template>
+            <template v-slot:item.location="{ item }">
+              <v-tooltip top>
+              <template v-if="item.location" v-slot:activator="{ on }">
+                <a v-on="on" class="d-block" target="_blank" :href="'https://maps.google.com?q=' +  item.location">{{ item.location.split('·')[0].trim() }}</a>
+                <span class="caption">{{ item.location.split('·').pop().trim() }}</span>
+              </template>
+              <span>Find in Google maps</span>
+              </v-tooltip>
+            </template>
+            <template v-slot:item.buyTicketHost="{ item }">
+              <template v-if="item.buyTicketHost">
+                <v-icon x-small="">mdi-ticket</v-icon>
+                <a :href="item.buyticketUrl" target="_blank">{{ item.buyTicketHost }}</a>
+              </template>
+              <template v-else-if="item.isFreeEvent">Free</template>
+              <template v-else>n/a</template>
+            </template>
+          </v-data-table>
+        </v-col>
       </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-btn
+            block
+            color="primary"
+            class="ma-2"
+            :loading="loading"
+            :disabled="loading"
+            @click="searchEvent({ append: true })"
+            v-if="this.events.items.length > 0 &&
+            this.events.hasNextPage &&
+            this.events.paginationCursor &&
+            !this.events.loading"
+              >Search For More Events</v-btn
+          >
+          <template v-if="this.events.loading && this.events.items.length > 0">
+            <div class="d-flex justify-center pb-2" >
+              <span class="">Searching for more events...</span>
+            </div>
+            <v-progress-linear indeterminate></v-progress-linear>
+          </template>
+          <v-alert v-if="this.events.items.length > 0 &&
+            !this.events.hasNextPage &&
+            !this.events.paginationCursor" type="info">
+           No more events to search.
+          </v-alert>
+        </v-col>
+        </v-row
+      >
     </v-flex>
+    <v-speed-dial v-if="this.events.items.length > 0"
+    absolute
+    fixed
+      v-model="actionButton.fab"
+      :top="actionButton.top"
+      :bottom="actionButton.bottom"
+      :right="actionButton.right"
+      :left="actionButton.left"
+      :direction="actionButton.direction"
+      :open-on-hover="actionButton.hover"
+      :transition="actionButton.transition"
+    >
+      <template v-slot:activator>
+        <v-btn
+          v-model="actionButton.fab"
+          color="yellow darken-3"
+          dark
+          fab
+        >
+          <v-icon v-if="actionButton.fab">mdi-close</v-icon>
+          <v-icon v-else>mdi-tune</v-icon>
+        </v-btn>
+      </template>
+    <v-tooltip left>
+        <template v-slot:activator="{on}">
+          <v-btn
+            v-on="on"
+            fab
+            dark
+            small
+            color="red"
+          >
+            <v-icon>mdi-counter</v-icon>
+          </v-btn>
+      </template>
+      <span>Statistics</span>
+      </v-tooltip>
+      <v-tooltip left>
+        <template v-slot:activator="{on}">
+          <v-btn
+            v-on="on"
+            fab
+            dark
+            small
+            color="red"
+          >
+            <v-icon>mdi-download</v-icon>
+          </v-btn>
+      </template>
+      <span>Download</span>
+      </v-tooltip>
+      <v-tooltip left>
+        <template v-slot:activator="{on}">
+          <v-btn
+          v-on="on"
+            fab
+            dark
+            small
+            color="indigo"
+          >
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+        </template>
+        <span>Filter events</span>
+      </v-tooltip>
+    </v-speed-dial>
   </v-layout>
 </template>
 
 <script>
 export default {
   watch: {
-    async loader() {
-      const l = this.loader
-      this[l] = !this[l]
-      setTimeout(() => (this[l] = false), 3000)
-      this.loader = null
-    },
     'city.search.keyword': function() {
       this.debouncedSearchCity()
-    },
-    dates: function() {
-      console.log(`start date! ${this.dates.start}`)
     }
   },
-  computed: {},
-
   methods: {
     buildQueryString: function() {
       const findCategoryId = categories => {
@@ -251,7 +345,6 @@ export default {
 
       this.city.search.loading = true
 
-      console.log(`searching for ${keyword}...`)
       this.$axios.$get(`/api/city?q=${keyword}`).then(res => {
         this.city.search.results = res
         this.city.search.loading = false
@@ -259,55 +352,27 @@ export default {
     },
     searchEvent: async function({ append = false }) {
       this.events.loading = true
-      console.log(
-        `Searching for events... ${this.$nuxt.$moment()} /api/events${this.buildQueryString()}`
-      )
 
       await this.$axios
         .$get(`/api/events${this.buildQueryString()}`)
         .then(response => {
-          console.log(response)
           const { events, paginationCursor, hasNextPage } = response
           if (append) this.events.items.push(...events)
           else this.events.items = events
           this.events.hasNextPage = hasNextPage
           this.events.paginationCursor = paginationCursor
           this.events.loading = false
-
-          console.log(this.events.items)
         })
     },
-    searchMore: async function() {
-      if (
-        this.events.items.length > 0 &&
-        this.events.hasNextPage &&
-        this.events.paginationCursor &&
-        !this.events.loading
-      ) {
-        console.log(`Searching for MORE events... ${this.$nuxt.$moment()}`)
-        await this.searchEvent({ append: true })
-      }
-    },
-    onScroll: function() {
-      const isAtTheBottomOfPage =
-        window.innerHeight + window.scrollY >= document.body.offsetHeight
-
-      if (isAtTheBottomOfPage) {
-        console.log(`bottom out!${this.$nuxt.$moment().toISOString()}`)
-        this.searchMore()
-      }
-    },
     findPage: async function({ eventId }) {
-
       const item = this.events.items.find(p => p.id == eventId)
       if(!item.host) return;
       this.$set(item.host, 'loading', true);
-      console.log('finding page...')
        await this.$axios.$get("/api/page/find?id=" + item.host.id).then(response => {
         this.$set(item.host, 'loading', false);
         if(!response) return;
         const {address = '', email = '', phone = '', website = ''} = response
-        console.log({email, phone, website, address})
+
         this.$set(item.host, 'details', {address, email, phone, website})
       });
     }
@@ -315,6 +380,19 @@ export default {
 
   data() {
     return {
+      actionButton: {
+        direction: 'top',
+        fab: false,
+        fling: false,
+        hover: false,
+        tabs: null,
+        top: false,
+        right: true,
+        bottom: true,
+        left: false,
+        transition: 'slide-y-reverse-transition'
+      },
+
       categories: {
         music: true,
         food: false,
@@ -347,7 +425,6 @@ export default {
       startDateMenu: false,
       startDate: '',
       endDate: '',
-      loader: null,
       loading: false
     }
   },
@@ -365,13 +442,8 @@ export default {
       .add(6, 'days')
       .format('YYYY-MM-DD')
 
-    console.log(JSON.parse(JSON.stringify(this.$data)))
-    console.log(`triggered created ${this.$nuxt.$moment()}`)
     this.debouncedSearchCity = this.$nuxt.$_.debounce(this.searchCity, 500)
-    window.addEventListener(
-      'scroll',
-      this.$nuxt.$_.debounce(this.onScroll, 500)
-    )
+
   }
 }
 </script>
